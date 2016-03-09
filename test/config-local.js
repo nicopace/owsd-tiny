2be344152dc0803dd1b159f790ef40dadfc39c81
Denis Osvald <denis.osvald@sartura.hr>
2016-03-09 19:42:47 +0100

    separate the tests for local and device
    Signed-off-by: Denis Osvald <denis.osvald@sartura.hr>

---
+++ test/config-local.js
@@ -0,0 +1,8 @@
+var config = {}
+module.exports = config
+
+config.url = "ws://127.0.0.1:8843"
+config.origin = "http://localhost/"
+config.test_file = "./test-local.txt"
+config.ubus_id = 1000
+config.protocol = 'ubus-json'
--- test/config.js
+++ test/config.js
@@ -1,8 +1,1 @@
-var config = {}
-module.exports = config
-
-config.url = "http://192.168.1.1:8843"
-config.origin = "http://localhost/"
-config.test_file = "./test.txt"
-config.ubus_id = 1000
-config.protocol = 'ubus-json'
+config-local.js
