ff0dd624f3aa087578463109bb4047769a6837ee
Denis Osvald <denis.osvald@sartura.hr>
2016-03-11 18:19:12 +0100

    add SSL support
    Signed-off-by: Denis Osvald <denis.osvald@sartura.hr>

--- src/main.c
+++ src/main.c
@@ -11,12 +11,24 @@
 
 #include <getopt.h>
 
+#ifndef WSD_DEF__PORT_NO
 #define WSD_DEF_PORT_NO 8843
+#endif
 
-#define WSD_DEF_CERT_PATH NULL
-#define WSD_DEF_PK_PATH NULL
+#define WSD_2str_(_) #_
+#define WSD_2str(_) WSD_2str_(_)
 
+#ifndef WSD_DEF_CERT_PATH
+#define WSD_DEF_CERT_PATH "/usr/share/owsd/cert.pem"
+#endif
+
+#ifndef WSD_DEF_PK_PATH
+#define WSD_DEF_PK_PATH "/usr/share/owsd/key.pem"
+#endif
+
+#ifndef WSD_DEF_UBUS_PATH
 #define WSD_DEF_UBUS_PATH "/var/run/ubus.sock"
+#endif
 
 struct prog_context global;
 
@@ -25,12 +37,15 @@ int main(int argc, char *argv[])
 	int rc = 0;
 
 	const char *ubus_sock_path = WSD_DEF_UBUS_PATH;
+	const char *ssl_cert_filepath = NULL;
+	const char *ssl_private_key_filepath = NULL;
 	int port = WSD_DEF_PORT_NO;
 	struct origin origin_list = { .list = LIST_HEAD_INIT(origin_list.list)};
 	char *error;
+	bool ssl_wanted = false;
 
 	int c;
-	while ((c = getopt(argc, argv, "s:p:o:h:")) != -1) {
+	while ((c = getopt(argc, argv, "s:p:o:c:k:h")) != -1) {
 		switch (c) {
 		case 's':
 			ubus_sock_path = optarg;
@@ -47,18 +62,57 @@ int main(int argc, char *argv[])
 			origin_el->url = optarg;
 			list_add_tail(&origin_el->list, &origin_list.list);
 			break;
+		case 'c':
+			ssl_wanted = true;
+			ssl_cert_filepath = optarg;
+			break;
+		case 'k':
+			ssl_wanted = true;
+			ssl_private_key_filepath = optarg;
+			break;
 		case 'h':
 		default:
 			fprintf(stderr,
 					"Usage: %s [ <options> ]\n"
-					"  -s <socket>         path to ubus socket\n"
-					"  -p <port>           port number\n"
-					"  -o <origin>	       origin url address\n"
+					"  -s <socket>      path to ubus socket [" WSD_DEF_UBUS_PATH "]\n"
+					"  -p <port>        port number [" WSD_2str(WSD_DEF_PORT_NO) "]\n"
+					"  -o <origin>      origin url address to whitelist\n"
+					"  -c <cert_path>   SSL cert path [" WSD_DEF_CERT_PATH "]\n"
+					"  -k <key_path>    SSL key path [" WSD_DEF_PK_PATH "]\n"
 					"\n", argv[0]);
 			return c == 'h' ? 0 : -2;
 		}
 	}
 
+	if (list_empty(&origin_list.list)) {
+		lwsl_warn("No origins whitelisted = reject all clients\n");
+	}
+
+	if (!ssl_cert_filepath) {
+		lwsl_info("Using default SSL cert path %s\n", WSD_DEF_CERT_PATH);
+		if (-1 == access(WSD_DEF_CERT_PATH, R_OK)) {
+			lwsl_warn("error opening default SSL cert: %s\n", strerror(errno));
+		} else {
+			ssl_cert_filepath = WSD_DEF_CERT_PATH;
+		}
+	}
+	if (!ssl_private_key_filepath) {
+		lwsl_info("Using default SSL key path %s\n", WSD_DEF_PK_PATH);
+		if (-1 == access(WSD_DEF_PK_PATH, R_OK)) {
+			lwsl_err("error opening default SSL key: %s\n", strerror(errno));
+		} else {
+			ssl_private_key_filepath = WSD_DEF_PK_PATH;
+		}
+	}
+	if (!ssl_cert_filepath || !ssl_private_key_filepath) {
+		lwsl_warn("SSL will not be used\n");
+		ssl_cert_filepath = ssl_private_key_filepath = NULL;
+		if (ssl_wanted) {
+			lwsl_err("SSL cert/keys setup error\n");
+			rc = 3;
+			goto no_ubus;
+		}
+	}
 
 	uloop_init();
 
@@ -83,8 +137,8 @@ int main(int argc, char *argv[])
 
 	lws_info.port = port;
 	lws_info.extensions = lws_get_internal_extensions();
-	lws_info.ssl_cert_filepath = WSD_DEF_CERT_PATH;
-	lws_info.ssl_private_key_filepath = WSD_DEF_PK_PATH;
+	lws_info.ssl_cert_filepath = ssl_cert_filepath;
+	lws_info.ssl_private_key_filepath = ssl_private_key_filepath;
 	lws_info.uid = -1;
 	lws_info.gid = -1;
 	lws_info.user = &global;
