8a9475bc913ccb31516e01aac73c5a0a90e0f216
Denis Osvald <denis.osvald@sartura.hr>
2016-03-14 12:09:14 +0100

    improve test script with SSL and events support
    Signed-off-by: Denis Osvald <denis.osvald@sartura.hr>

--- test/client.js
+++ test/client.js
@@ -6,96 +6,151 @@ var WebSocket = require('ws');
 
 var session_id;
 var ubus_id = config.ubus_id;
+
 var tests = [];
-var i = 0;
-var j = 0;
-var session_json='{"jsonrpc": "2.0", "id": UBUS_ID, "method": "call", "params": [ "00000000000000000000000000000000", "session", "login", { "username": "root", "password": "root"}]}'
+var tests_by_id = [];
+
+var events = [];
+
+var read_events = 0;
+
+var sent_tests = 0;
+var recv_tests = 0;
+
+var session_json='{"jsonrpc": "2.0", "id": UBUS_ID, "method": "call", "params": [ "00000000000000000000000000000000", "session", "login", { "username": "owsd_test", "password": "x"}]}'
 
 var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream(config.test_file)
+	input: require('fs').createReadStream(config.test_file)
 });
 
 function strncmp(str1, str2, n) {
-    str1 = str1.substring(0, n);
-    str2 = str2.substring(0, n);
-    return ( ( str1 == str2 ) ? 1 : 0);
+	str1 = str1.substring(0, n);
+	str2 = str2.substring(0, n);
+	return ( ( str1 == str2 ) ? 1 : 0);
 }
 
-console.log("startig server.js script");
+console.log("startig test script");
 
 
 process.argv.forEach(function (val, index, array) {
-    if (index == 2) {
-        session_id = val;
-        console.log("session id is:" + session_id);
-    }
+	if (index == 2) {
+		session_id = val;
+		console.log("session id is:" + session_id);
+	}
 });
 
+if (typeof session_id === 'undefined') {
+	session_json = session_json.replace(/UBUS_ID/g, ubus_id++)
+		var session_json_obj = JSON.parse(session_json);
+	var tmp = { req_line: session_json, obj: session_json_obj, resp_line: "" };
+	tests.push(tmp);
+}
+
+var reading_req_line = true;
+var reading_event = false;
+
 lineReader.on('line', function (line) {
-    if (line.length > 0 && line.substring(0, 1) !== '#') {
-        tests[i] = line;
-        i = i + 1;
-    }
+	if (line.length == 0 || line.charAt(0) === '#')
+		return;
+
+	line = line.replace(/UBUS_ID/g, ubus_id);
+
+	if (reading_req_line) {
+		reading_req_line = false;
+
+		if (line.charAt(0) === "+") {
+			//console.log(chalk.blue(" EVENT "));
+			var curr_event = { req_line: line, obj: null}
+			events.push(curr_event);
+			reading_event = true;
+		} else {
+			// what we send
+			try {
+				var obj = JSON.parse(line);
+				var curr_test = {req_line: line, obj: obj};
+				tests_by_id[obj.id] = curr_test;
+				tests.push(curr_test);
+			} catch (e) {
+				// skip invalid test
+				console.log("skip line with error" + e);
+				reading_req_line = true;
+				return;
+			}
+		}
+	} else {
+		if (reading_event) {
+			events[events.length-1].resp_line = line;
+		} else {
+			tests[tests.length-1].resp_line = line;
+			++ubus_id;
+		}
+		reading_event = false;
+		reading_req_line = true;
+	}
 });
 
 var ws = new WebSocket(config.url, {
-    protocol: config.protocol,
-    origin: config.origin
+	protocol: config.protocol,
+	rejectUnauthorized: false,
+	origin: config.origin
 });
 
 ws.on('open', function open() {
-    console.log('connected');
-    if ( typeof session_id === 'undefined') {
-        var msg = session_json.replace(/UBUS_ID/g, ubus_id);
-        ws.send(msg, {mask: true});
-        console.log("> " + msg);
-    } else {
-       ubus_id = ubus_id + 1;
-       var msg = tests[j].replace(/SESSION_ID/g, session_id).replace(/UBUS_ID/g, ubus_id);
-       ws.send(msg, {mask: true});
-       console.log("> " + msg);
-       j = j + 2;
-    }
+	console.log('connected');
+	var msg = tests[sent_tests++].req_line;
+	ws.send(msg, {mask: true});
+	console.log("> " + msg);
 });
 
-ws.on('close', function close() {
-    console.log('disconnected');
+ws.on('close', function close(e) {
+	console.log('disconnected ' + e);
 });
 
-ws.on('error', function close() {
-    console.log('error');
+ws.on('error', function close(e) {
+	console.log('error ' + e);
 });
 
 ws.on('message', function message(data, flags) {
+	var obj = JSON.parse(data);
+	if (typeof session_id === 'undefined' && recv_tests == 0) {
+		console.log(data);
+		session_id = obj.result[1].ubus_rpc_session;
+		console.log(chalk.blue("LOGIN " + session_id));
+		++recv_tests;
+	} else {
+		if (typeof obj.id === 'undefined') {
+			// we received event
+			var compare = events[read_events++].resp_line.replace(/SESSION_ID/g, session_id);
+			//console.log(chalk.magenta("event"));
+		} else {
+			var compare = tests_by_id[obj.id].resp_line.replace(/SESSION_ID/g, session_id);
+			//console.log(chalk.yellow("resp"));
+			++recv_tests;
+		}
 
-    if ( typeof session_id === 'undefined') {
-        session_id = data.split("\"")[11]
-    } else {
-        var compare = tests[j - 1].replace(/SESSION_ID/g, session_id).replace(/UBUS_ID/g, ubus_id);
+		if (strncmp(data, compare, compare.length )) {
+			console.log(chalk.green("PASS"));
+		} else {
+			console.log(chalk.red("FAIL"));
+			console.log("got");
+			console.log(chalk.red(data));
+			console.log("expected");
+			console.log(chalk.green(compare));
+		}
+	}
 
-        if (strncmp(data, compare, compare.length )) {
-            console.log(chalk.green("PASS"));
-        } else {
-            console.log(chalk.red("FAIL"));
-            console.log("got");
-            console.log(chalk.red(data));
-            console.log("expected");
-            console.log(chalk.green(tests[j-1].replace(/SESSION_ID/g, session_id).replace(/UBUS_ID/g, ubus_id)));
-        }
+	console.log();
+	console.log();
 
-            console.log();
-            console.log();
-    }
-   if (j < i) {
-       ubus_id = ubus_id + 1;
-       var msg = tests[j].replace(/SESSION_ID/g, session_id).replace(/UBUS_ID/g, ubus_id);
-       ws.send(msg, {mask: true});
-       console.log("> " + msg);
-   }
-
-   if (j >= (i - 1)) {
-       ws.close()
-   } else {
-       j = j + 2;
-   }
+	if (recv_tests == tests.length && read_events == events.length) {
+		ws.close();
+	} else {
+		if (sent_tests === recv_tests) {
+			var msg = tests[sent_tests++].req_line.replace(/SESSION_ID/g, session_id);
+			ws.send(msg, {mask: true});
+			console.log("> " + msg);
+		} else {
+			console.log("+ ");
+		}
+	}
 });
--- test/config-openwrtdevice.js
+++ test/config-openwrtdevice.js
@@ -1,8 +1,8 @@
 var config = {}
 module.exports = config
 
-config.url = "http://192.168.1.1:8843"
+config.url = "https://192.168.1.1:8843"
 config.origin = "http://localhost/"
-config.test_file = "./test.txt"
+config.test_file = "./test-openwrtdevice.txt"
 config.ubus_id = 1000
 config.protocol = 'ubus-json'
