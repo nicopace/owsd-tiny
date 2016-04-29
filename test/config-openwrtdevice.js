18825d54a38d137fb26f418d1d2f7bb2991eaf89
Denis Osvald <denis.osvald@sartura.hr>
2016-04-29 15:58:46 +0200

    add more tests
    Signed-off-by: Denis Osvald <denis.osvald@sartura.hr>

--- test/config-openwrtdevice.js
+++ test/config-openwrtdevice.js
@@ -2,7 +2,7 @@ var config = {}
 module.exports = config
 
 config.url = "https://192.168.1.1:8843"
-config.origin = "http://localhost/"
+config.origin = "https://192.168.1.1:8843"
 config.test_file = "./test-openwrtdevice.txt"
 config.ubus_id = 1000
 config.protocol = 'ubus-json'
